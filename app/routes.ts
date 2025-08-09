cbf3a89594f4acb901bdd9df5bf0d93f789e7244
Alex Tsui <alextsui@pm.me>
2025-08-09 21:07:16 +0000

    Setup github action to deploy github page

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    ...prefix("/primes",
+        [index("routes/home.tsx")]
+    )
+] satisfies RouteConfig;
